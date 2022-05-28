77ce252c8287a05c9092db730e7119b4ec2d1921
shamimulhaque162 <khandoker15-1992@diu.edu.bd>
2022-05-28 11:57:15 +0600

    ok so far

--- src/Pages/Notfound/Notfound.js
+++ src/Pages/Notfound/Notfound.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const Notfound = () => {
   return (
-    <div className="container text-center flex flex-col items-center">
-      <h1 className="text-center">404 not found</h1>
+    <div className="container text-center flex flex-col items-center mx-auto mb-20">
+      <h1 className="text-center text-error">404 not found</h1>
       <img
         className="w-75"
         src="https://miro.medium.com/max/1400/1*DeBkx8vjbumpCO-ZkPE9Cw.png"
