72982f98e0e84efba5810388458b545dd11005f9
shamimulhaque162 <khandoker15-1992@diu.edu.bd>
2022-09-04 16:16:02 +0600

    map attribution and url changed

--- src/Pages/Home/BasicInfo/GoogleMapContainer/osm-providers.js
+++ src/Pages/Home/BasicInfo/GoogleMapContainer/osm-providers.js
@@ -1,8 +1,8 @@
 const mapCredintial= {
   maptailer: {
-    url: "https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=Bz61Cdnn3mq0EaoY91gm",
+    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     attribution:
-      '&copy; <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
 };
 export default mapCredintial;
